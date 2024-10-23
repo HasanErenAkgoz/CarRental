2caade67cc73e83799bf8d44e00aa5829b63240a
Hasan Eren Akgöz <hasanerenakgoz@hotmail.com>
2024-10-23 23:27:47 +0300

    First Commit

--- Persistence/PersistanceServicesRegistration.cs
+++ Persistence/PersistanceServicesRegistration.cs
@@ -1,15 +1,9 @@
 ﻿using Application.Services.Repositories;
-using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Context;
 using Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence
 {
